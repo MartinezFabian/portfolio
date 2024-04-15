357a8e6030b2ec5f4d13379fcdc9ee592cb49054
MartinezFabian <FabianMartinezDev@gmail.com>
2024-04-14 22:25:21 -0300

    Add styles of Project.jsx in Projects.module.css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,48 +1,54 @@
 import styles from './Project.module.css';
+import { Chrome } from './icons/Chrome';
+import { Github } from './icons/Github';
 
 export const Project = ({ id, title, description, images, technologies, github, demo }) => {
   return (
     <article className={styles.project}>
-      <picture>
+      <picture className={styles.project__picture}>
         <source srcSet={images[0]} type="image/avif" />
         <source srcSet={images[1]} type="image/webp" />
         <source srcSet={images[2]} type="image/jpeg" />
         <img
-          className={styles.project__img}
           loading="lazy"
           decoding="async"
           src={images[2]}
           alt={title + ' image'}
           width="500"
           height="300"
+          className={styles.project__img}
         />
       </picture>
 
-      <h3 className={styles.project__title}>{title}</h3>
-      <p className={styles.project__description}>{description}</p>
+      <div className={styles.project__content}>
+        <h3 className={styles.project__title}>{title}</h3>
+        <p className={styles.project__description}>{description}</p>
 
-      <div className={styles.project__technologies}>
-        {technologies.map((technology) => {
-          return (
-            <p key={technology + id} className={styles.project__technology}>
-              {technology}
-            </p>
-          );
-        })}
-      </div>
+        <div className={styles.project__technologies}>
+          {technologies.map((technology) => {
+            return (
+              <p key={technology + id} className={styles.technology}>
+                {technology}
+              </p>
+            );
+          })}
+        </div>
 
-      <div className={styles.project__links}>
-        {github.length > 0 ? (
-          <a href={github} target="_blank" rel="noreferrer" className={styles.project__link}>
-            Github
-          </a>
-        ) : null}
+        <div className={styles.project__links}>
+          {github.length > 0 ? (
+            <a href={github} target="_blank" rel="noreferrer" className={styles.project__link}>
+              Github
+              <Github width="16" height="16"></Github>
+            </a>
+          ) : null}
 
-        {demo.length > 0 ? (
-          <a href={demo} target="_blank" rel="noreferrer" className={styles.project__link}>
-            Demo
-          </a>
-        ) : null}
+          {demo.length > 0 ? (
+            <a href={demo} target="_blank" rel="noreferrer" className={styles.project__link}>
+              Demo
+              <Chrome width="16" height="16"></Chrome>
+            </a>
+          ) : null}
+        </div>
       </div>
     </article>
   );
